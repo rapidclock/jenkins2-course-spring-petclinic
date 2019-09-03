44ebd2ff1c0e378c3b8dc7647770dae282dc1cab
Rahul Thomas <rapidclock@users.noreply.github.com>
2019-09-03 14:28:50 -0600

    Making a test failure 1

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -38,7 +38,8 @@ public class ValidatorTests {
 
         assertThat(constraintViolations.size()).isEqualTo(1);
         ConstraintViolation<Person> violation = constraintViolations.iterator().next();
-        assertThat(violation.getPropertyPath().toString()).isEqualTo("firstName");
+        // assertThat(violation.getPropertyPath().toString()).isEqualTo("firstName");
+        assertThat(violation.getPropertyPath().toString()).isEqualTo("asda");
         assertThat(violation.getMessage()).isEqualTo("may not be empty");
     }
 
